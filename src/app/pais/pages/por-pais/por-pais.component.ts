c574ec28c04467305d9bec97fe7d10bb61c65a41
Manuel Gutierrez <magutbar@gmail.com>
2023-03-17 10:07:35 +0100

    PaisApp terminada

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,19 +1,59 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
 
-  termino: string = '';
+  termino : string = '';
+  hayError: boolean = false;
+  paises  : Country[] = [];
 
-  constructor() { }
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
-  buscar() {
-    console.log(this.termino);
+  constructor(private paisService: PaisService) { }
+
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe((paises) => {
+        this.paises = paises;
+
+      }, (err) => {
+        this.hayError = true;
+        this.paises   = [];
+      });
+
+  }
+
+  sugerencias(termino: string){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino)
+      .subscribe(
+        paises => this.paisesSugeridos = paises.splice(0,5),
+        (err) => this.paisesSugeridos = []
+      );
+
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+    //this.mostrarSugerencias = false;
   }
 
 
